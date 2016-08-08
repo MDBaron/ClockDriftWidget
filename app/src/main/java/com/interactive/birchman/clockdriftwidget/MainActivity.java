b90df622d2851c4eeeac40a4ec97ad6be7e2856f
MDBaron <mbaron@uga.edu>
2016-08-07 23:12:52 -0400

    Fixed the file IO issue, email intent is drawing the correct data now...

--- app/src/main/java/com/interactive/birchman/clockdriftwidget/MainActivity.java
+++ app/src/main/java/com/interactive/birchman/clockdriftwidget/MainActivity.java
@@ -1,6 +1,8 @@
 package com.interactive.birchman.clockdriftwidget;
 
+import android.content.Intent;
 import android.graphics.Color;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +10,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.text.SimpleDateFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,9 +26,13 @@ public class MainActivity extends AppCompatActivity {
     double sysTimeDiff = 0.0;
     double sysTime = 0.0;
     double netTime = 0.0;
+    double timeDev = 0.0;
 
     String timeString = "";
     String timeColor = "#9E9E9E";
+    private String fileName = "Clock_Log";
+
+    FileOutputStream fos = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,32 +66,65 @@ public class MainActivity extends AppCompatActivity {
         SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
         timeString = df.format(sysTime);
         //Switch statement for text Color selection according to "Drift"
+        timeDev = sysTimeDiff;
         sysTimeDiff = sysTimeDiff/1000/60;
         if(sysTimeDiff > 5){
-            timeColor = "#F44336";// Red -> More than 5 minutes
+            // Red -> More than 5 minutes
+            timeColor = "#F44336";
         } else if(sysTimeDiff < 5 && sysTimeDiff > 1){
-            timeColor = "#4CAF50";// Green -> Less than 5 minutes
+            // Green -> Less than 5 minutes
+            timeColor = "#4CAF50";
         } else if(sysTimeDiff < 1 && sysTimeDiff > 0){
-            timeColor = "#FFEB3B";// Yellow -> Less than a minute
+            // Yellow -> Less than a minute
+            timeColor = "#FFEB3B";
         } else {
-            timeColor = "#9E9E9E";// Gray -> Unknown
+            // Gray -> Unknown
+            timeColor = "#9E9E9E";
         }
-        setTimeUI(timeString, timeColor);
+        setTimeUI(timeString, timeColor, Double.toString(timeDev));
     }
 
-    public void setTimeUI(String time, String color) {
+    public void setTimeUI(String time, String color, String deviation) {
     final String t = time;
     final String c = color;
+        final String d = deviation;
     runOnUiThread(new Runnable() {
         @Override
         public void run () {
             TextView currentTime = (TextView) findViewById(R.id.current_time);
             currentTime.setText(String.valueOf(t));
             currentTime.setBackgroundColor(Color.parseColor(c));
+            TextView logView = (TextView) findViewById(R.id.log_View);
+            String log = "Current Clock drift of " + d + "milliseconds at " + t + "\n";
+            logView.setText(log);
+            storeLog(log);
         }
     }
     );
 }
+
+
+    public void storeLog(String str){
+        //TO-DO: Split stored data with /n
+
+        try {
+            fos = openFileOutput(fileName, MODE_APPEND);
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+
+        //Output Stream appends each script to byte-wise file
+        OutputStreamWriter osw = new OutputStreamWriter(fos);
+        try {
+            osw.write(str + "\n");
+            osw.flush();
+            osw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
     public void fudgeTime(View view){
         //Add a substantial time event
         offset = 1000000;
@@ -93,6 +138,7 @@ public class MainActivity extends AppCompatActivity {
         offset = 0.0;
         sysTimeDiff = 0.0;
         netTime = 0.0;
+        timeDev = 0.0;
         sysTime = System.currentTimeMillis();
         //Reset Timer status attributes
         SimpleDateFormat df = new SimpleDateFormat("HH:mm:ss");
@@ -106,7 +152,42 @@ public class MainActivity extends AppCompatActivity {
         butt.setText("Begin Polling");
         mTask.cancel(true);
         //Reset Timer status
-        setTimeUI(timeString, timeColor);
+        setTimeUI(timeString, timeColor, Double.toString(timeDev));
+    }
+
+    public void transmit(View view){
+        //Pull data from internal device storage and email
+
+        String logData = "";
+        StringBuffer sb = new StringBuffer("");
+
+        try {
+            FileInputStream fis = openFileInput("Clock_Log");
+            InputStreamReader isr = new InputStreamReader(fis);
+            BufferedReader br = new BufferedReader(isr);
+
+            String inputStr = br.readLine();
+            while(inputStr != null){
+                sb.append(inputStr);
+                inputStr = br.readLine();
+            }
+
+            isr.close();
+            fis.close();
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+        logData = sb.toString();
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_EMAIL,new String[] { "tehmatty86@gmail.com" });
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Clock Sync Data" );
+        intent.putExtra(Intent.EXTRA_TEXT, logData);
+        if(intent.resolveActivity(getPackageManager()) != null){
+            startActivity(intent);
+        }
     }
 
 
@@ -132,7 +213,7 @@ public class MainActivity extends AppCompatActivity {
                 //Delay loop arbitrarily to prevent constant processing
                 setTime(timeString, timeColor);
                 try {
-                    Thread.sleep(10000);
+                    Thread.sleep(1000);
                 } catch (InterruptedException e) {
                     Thread.interrupted();
                 }
